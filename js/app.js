7699a9b26f53bb04994c4111a405a2a27a8f6933
Sarah Thiemann <sjtmann@hotmail.com>
2017-07-14 19:57:12 -0400

    traverse the earthe
    adjusted width % in my media querie and no longer have the white soace issue

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@ function myFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
 }
 
-// Close the dropdown if the user clicks outside of it
+// Close the dropdown if the user clicks outside of it-->
 window.onclick = function(e) {
   if (!e.target.matches('.dropbtn')) {
     var myDropdown = document.getElementById("myDropdown");
@@ -13,4 +13,8 @@ window.onclick = function(e) {
       }
   }
 }
-// submitt button for contact form-->
+
+//-- submitt button for contact form-->
+function mycode () {
+
+}
